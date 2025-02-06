171a3a19111f54868cb2d867c14ccd9775f12e82
Jawaid Aziz <javaidmeon24@gmail.com>
2025-02-07 01:14:31 +0500

    Nodejs: Handling URLs

--- server/index.js
+++ server/index.js
@@ -2,9 +2,13 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log("Request received");
-  res.end('Hello Server');
+  if(req.url === '/next')
+  {
+    res.end("This is for the NEXT path");
+  }
 });
 
 server.listen(8000, () => {
   console.log('Server is listening on port 8000');
-}); 
+});
+
