89aba581fb13b54340c04eaa3ac37f86cc87f3d0
Jawaid Aziz <javaidmeon24@gmail.com>
2025-02-07 23:51:35 +0500

    REST APIs

--- server/index.js
+++ server/index.js
@@ -1,17 +1,41 @@
-const http = require('http');
 const express = require("express");
-
+const fs = require("fs");
+const users = require("./MOCK_DATA.json");
 const app = express();
+const port  = 8000;
 
-app.get("/", (req, res) => {
-    return res.send("Hello for home page")
+app.use(express.urlencoded({extended: true}));
+
+app.get("/users", (req, res) => {
+    res.json(users);
 })
 
-app.get("/about", (req, res) => {
-    return res.send("Hello for about page")
+app.post("/api/users", (req, res) => {
+    const body = req.body;
+    users.push({id: users.length + 1, ...body});
+    fs.writeFileSync("./MOCK_DATA.json", JSON.stringify(users), (err,data) => {
+        res.json("User added");
+    });
 })
 
-app.listen(8000, () => {
+app.route("/users/:id")
+.get((req, res) => {
+    const id = req.params.id;
+    const user = users.find(user => user.id == id);
+    res.json(user);
+})
+.patch((req, res) => {
+
+})
+.delete((req, res) => {
+    const id = req.params.id;
+    const index = users.findIndex(user => user.id == id);
+    users.splice(index, 1);
+    fs.writeFileSync("./MOCK_DATA.json", JSON.stringify(users));
+    res.json("User deleted");
+})
+
+app.listen(port, () => {
     console.log("Server is listening");
 })
 
